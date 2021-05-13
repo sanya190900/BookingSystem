29ce3a5441bf4f7584cbe908a5425d4febde3c42
alex190900 <sanya190900@gmail.com>
2021-05-13 16:04:23 +0200

    Angular: page responsibility

--- bookingsystemUI/src/app/services/user/user.service.ts
+++ bookingsystemUI/src/app/services/user/user.service.ts
@@ -12,8 +12,6 @@ export class UserService {
   constructor(private httpClient:HttpClient) { }
 
   register(user : UserModel) : Observable<UserModel> {
-    console.log(apiPath + 'user/');
-    console.log(user);
     return this.httpClient.post<UserModel>(apiPath + 'user/', user);
   }
 }
